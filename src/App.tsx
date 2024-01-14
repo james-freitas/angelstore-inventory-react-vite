e4a004016ffa9d5b5d165d827bd4913a5088ef36
James Freitas <jamesfrj@yahoo.com.br>
2024-01-13 22:56:04 -0300

    Fetch all categories from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import { useCategoryData } from './hooks/useCategoryData';
 
 function App() {
+  const { data } = useCategoryData();
+
   return (
     <div className="container">
-      <h1>Application Ready</h1>
+      <h1>Categorias</h1>
+      <table>
+      <thead>
+      <tr>
+          <th>Name</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+          <td>
+            {data?.map(categoryData => categoryData.name)}
+          </td>
+      </tr>
+      </tbody>
+    </table>      
     </div>
   )
 }
